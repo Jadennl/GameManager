360d80b7ba0773684d77c2008104b6710c80f2e0
LeonarJN20 <LeonarJN20@BFHS-415244.pwcs.edu>
2018-05-22 09:38:34 -0400

    More work on the menu, added getter and setter to Player

--- Eclipse/src/PlayerManager.java
+++ Eclipse/src/PlayerManager.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class PlayerManager {
-private List<Player> players = new ArrayList<Player>();
+	private static List<Player> players = new ArrayList<Player>();
 
 	public static void main(String[] args) {
 		System.out.println("Game Manager!");
@@ -11,16 +11,15 @@ private List<Player> players = new ArrayList<Player>();
 
 	public static void ask() {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("\n\t\t MENU" + "\n(1) Create Character \n(2) Manage Character(s)");
+		System.out.println("\n\t\t MAIN MENU" + "\n(1) Create Player \n(2) Manage Player(s)");
 		System.out.println("\tPlease type your option: ");
 		try {
 			int option = sc.nextInt();
-			sc.close();
-			if (option == 1)
+			if (option == 1) {
 				createPlayer();
-			else if (option == 2)
-				System.out.println("bar");
-			else
+			} else if (option == 2) {
+				manage();
+			} else
 				ask();
 		} catch (InputMismatchException e) {
 			System.out.println("ERROR: Please use an integer ");
@@ -28,22 +27,36 @@ private List<Player> players = new ArrayList<Player>();
 		}
 	}
 	
-	public Player createPlayer() {
-		Scanner sc = new Scanner(System.in); 
+	public static void manage(){
+		Scanner mscan = new Scanner(System.in);
 		System.out.println("\n\t\tPLAYER MENU");
-		System.out.println("(1) New Basic Character \n(2) New Advanced Character");
+		System.out.println("There are: " + players.size() + " players");
+		System.out.println("(1) Player List \n(2) Manage Player \n(3) Delete Player");
+	}
+
+	public static void createPlayer() {
+		Scanner pscan = new Scanner(System.in);
+		System.out.println("\n\t\tCREATE PLAYER");
+		System.out.println("(1) New Basic Player \n(2) New Advanced Player \n(3) return to main menu");
 		try {
-			int option = sc.nextInt();
-			sc.close();
-			if (option == 1)
-				createPlayer();
-			else if (option == 2)
-				System.out.println("bar");
-			else
+			int option = pscan.nextInt();
+			if (option == 1) {
+				System.out.println("What is the players name?");
+				String name = pscan.next();
+				Player p = new Player(name);
+				System.out.println(p.getName() + " was created.");
+				players.add(p);
 				ask();
+			} else if (option == 2)
+				System.out.println("bar");
+			else if(option == 3)
+				ask();
+			else
+				createPlayer();
 		} catch (InputMismatchException e) {
 			System.out.println("ERROR: Please use an integer ");
-			ask();
+			createPlayer();
 		}
+
 	}
 }
