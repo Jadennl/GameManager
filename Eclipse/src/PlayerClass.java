c0ccc69742dbf7a4bff88f671e7f8b9ef6dfc9d7
LeonarJN20 <LeonarJN20@BFHS-415244.pwcs.edu>
2018-05-23 08:15:19 -0400

    Working on classes and moving around variables.

--- Eclipse/src/PlayerClass.java
+++ Eclipse/src/PlayerClass.java
@@ -1,10 +1,25 @@
 
 public abstract class PlayerClass {
 	private String className;
+	private int armor;
+	private int mana;
+	private int speed;
+	private double maxHealth;
 
-	public PlayerClass(String player, String pclass) {
+	public PlayerClass(String pclass, int armor, int mana, int speed, double maxHealth) {
 		className = pclass;
 	}
 
-	
+	public int[] getStats() {
+		int[] stats = new int[3];
+		stats[0] = armor;
+		stats[1] = mana;
+		stats[2] = speed;
+		return stats;
+	}
+
+	public double getMax() {
+		return maxHealth;
+	}
+
 }
